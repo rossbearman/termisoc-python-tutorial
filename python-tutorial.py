0859aa103dada464e91aaf0b6724f68961119fb0
Ross Bearman <ross@rossbearman.co.uk>
2011-03-14 19:10:58 +0000

    Initial commit.

---
+++ python-tutorial.py
@@ -0,0 +1,20 @@
+import random
+
+class Player:
+	level = 1
+
+class Output:
+	def printHelp(self):
+		print "Known commands are:\n"
+		print "quit\n"
+		print "help\n"
+
+player = Player()
+
+while(True):
+	userInput = raw_input("Please enter a command:")
+	
+	if userInput == 'quit':
+		quit()
+	if userInput == 'help':
+		output.printHelp()
