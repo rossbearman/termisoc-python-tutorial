e5dc019e5e8d6f1d2ca5843297a8c9142a902422
Ross Bearman <ross@rossbearman.co.uk>
2011-03-14 21:27:49 +0000

    Added areas, consumables and inventories

--- python-tutorial.py
+++ python-tutorial.py
@@ -2,10 +2,49 @@ import random
 
 class Player:
 	level = 1
+	health = 10
+
+	area = Area("Test", [Potion("Test Potion"), Poison("Test Poison")])
+	inventory = []
+
+	def pickupItem(index):
+		area.inventory[index].
+		inventory.append(area.inventory[index])
+		del area.inventory[index]
+	
+	def useItem(index)
+		inventory[index].consume(self)
+		del inventory[index]
+
+class Area:
+	inventory = []
+
+	def __init__(self, name, inventory):
+		self.name = name
+		self.inventory = inventory
+
+	
+class Consumable:
+	def __init__(self, name):
+		self.name = name
+		
+	def consume(player):
+		player.level += 1
+
+class Potion(Consumable):
+	def consume(player):
+		player.health += 1
+
+class Poison(Consumable):
+	def consume(player):
+		player.health -= 1
 
 class Output:
 	def printHelp(self):
 		print "Known commands are:"
+		print "inventory"
+		print "pickup <item>"
+		print "use <item>"
 		print "quit"
 		print "help"
 
@@ -14,8 +53,15 @@ output = Output()
 
 while(True):
 	userInput = raw_input("Please enter a command:")
-	
-	if userInput == 'quit':
+	userInput = userInput.split()
+
+	if userInput[0] == 'inventory'
+		player.displayInventory()
+	elif userInput[0] == 'pickup':
+		player.pickupItem(userInput[1])
+	elif userInput[0] == 'use':
+		player.useItem(userInput[1])
+	elif userInput[0] == 'quit':
 		quit()
-	if userInput == 'help':
+	elif userInput[0] == 'help':
 		output.printHelp()
