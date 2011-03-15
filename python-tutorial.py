d1f3dc3ab1e9c4e8454b8104f0787bebaaf064c1
Ross Bearman <ross@rossbearman.co.uk>
2011-03-15 00:37:31 +0000

    Working items and areas, pickup and use with potions and poisons.

--- python-tutorial.py
+++ python-tutorial.py
@@ -1,21 +1,5 @@
 import random
 
-class Player:
-	level = 1
-	health = 10
-
-	area = Area("Test", [Potion("Test Potion"), Poison("Test Poison")])
-	inventory = []
-
-	def pickupItem(index):
-		area.inventory[index].
-		inventory.append(area.inventory[index])
-		del area.inventory[index]
-	
-	def useItem(index)
-		inventory[index].consume(self)
-		del inventory[index]
-
 class Area:
 	inventory = []
 
@@ -23,21 +7,54 @@ class Area:
 		self.name = name
 		self.inventory = inventory
 
-	
+	def displayInventory(self):
+		i = 0
+		print "You look around, and see:"
+		for item in self.inventory:
+			print str(i) + ": " + item.name
+			i += 1
+
 class Consumable:
 	def __init__(self, name):
 		self.name = name
 		
-	def consume(player):
+	def consume(self, player):
 		player.level += 1
 
 class Potion(Consumable):
-	def consume(player):
+	def consume(self, player):
 		player.health += 1
 
 class Poison(Consumable):
-	def consume(player):
+	def consume(self, player):
 		player.health -= 1
+
+class Player:
+	level = 1
+	health = 10
+
+	area = Area("Test", [Potion("Test Potion"), Poison("Test Poison")])
+	inventory = []
+
+	def pickupItem(self, index):
+		self.area.inventory[index]
+		self.inventory.append(self.area.inventory[index])
+		del self.area.inventory[index]
+	
+	def useItem(self, index):
+		self.inventory[index].consume(self)
+		del self.inventory[index]
+	
+	def displayInventory(self):
+		i = 0
+		print "Your inventory:"
+		for item in self.inventory:
+			print str(i) + ": " + item.name
+			i += 1
+	
+	def displayStatus(self):
+		print "Level: " + str(self.level)
+		print "Health: " + str(self.health)
 
 class Output:
 	def printHelp(self):
@@ -55,13 +72,25 @@ while(True):
 	userInput = raw_input("Please enter a command:")
 	userInput = userInput.split()
 
-	if userInput[0] == 'inventory'
+	if userInput[0] == 'inventory' or userInput[0] == 'i':
 		player.displayInventory()
-	elif userInput[0] == 'pickup':
-		player.pickupItem(userInput[1])
-	elif userInput[0] == 'use':
-		player.useItem(userInput[1])
-	elif userInput[0] == 'quit':
+	elif userInput[0] == 'status' or userInput[0] == 's':
+		player.displayStatus()
+	elif userInput[0] == 'pickup' or userInput[0] == 'p':
+		try:
+			player.pickupItem(int(userInput[1]))
+		except:
+			print "That item doesn't exist!"
+	elif userInput[0] == 'use' or userInput[0] == 'u':
+		try:
+			player.useItem(int(userInput[1]))
+		except:	
+			print "That item doesn't exist!"
+	elif userInput[0] == 'look' or userInput[0] == 'l':
+		player.area.displayInventory()
+	elif userInput[0] == 'quit' or userInput[0] == 'q':
 		quit()
-	elif userInput[0] == 'help':
+	elif userInput[0] == 'help' or userInput[0] == 'h':
 		output.printHelp()
+	else:
+		print "Uh oh! I can't find the command '" + userInput[0] + "', please enter another command."
